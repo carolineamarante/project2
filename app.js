d33f8868f7aee8386e9b71986e0fa8b94d6a7e4f
Caroline S. Amarante <caroline.amarante1998@gmail.com>
2022-07-14 09:48:11 -0300

    comitando de novo

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 const http = require('http'); 
-const fs = require('fs'); 
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"name="message"><button type="submit">Confirm</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    } 
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-       
-        });
-        
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>PRIMEIRA BAGAÇA CARAIO</title><head>');
-    res.write('<body><h1>FALA MEMO ISAQUINHO TE AMO</hq></body>');
-    res.write('</html>');
-    res.end();
-}); 
+const server = http.createServer(routes); 
 
 
 server.listen(3000);
