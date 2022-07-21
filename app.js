539643baaaaf87c5d95d759f2b349b3ab02df2ea
Caroline S. Amarante <caroline.amarante1998@gmail.com>
2022-07-21 11:09:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('middle of the night');
-    next();
+app.use('/produtinho',(req, res, next) => {
+    console.log('middle of the night 2')
+    res.send('aqui voce vende produtinho')
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('middle of the night 2')
-    //...
+    res.send('pipipi popopo')
 });
 
 app.listen(3000);
