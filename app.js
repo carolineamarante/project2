ed68aa0069063650a9736b3f763e64f307f6ba77
Caroline S. Amarante <caroline.amarante1998@gmail.com>
2022-07-22 12:16:51 -0300

    .

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const http = require('http'); 
+//calling functions
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/produtinho',(req, res, next) => {
-    console.log('middle of the night 2')
-    res.send('aqui voce vende produtinho')
-});
+//routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-    console.log('middle of the night 2')
-    res.send('pipipi popopo')
-});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//calling pages
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+// o caminho vazio / precisa sempre ficar por ultimo na lista, caso contrário as outras paginas
+// nunca serão executadas, isso por iniciarem com a /
+
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = express.Router();
+
+
+/*
+neste caso, como o routes está com o get, ao invés de use, o redirecionamento da pág vai funcionar normal
+porque só vai pegar files que contenham um GET, se usasse o routes.use, aí sim a ordem iria fazer diferença no 
+arquivo app.js. 
+
+USE - a ordem das rotas importa
+GET - tanto faz a ordem, ele vai funcionar certinho
+*/
+routes.get('/', (req, res, next) => {
+    res.send('Pagina Principal que cuida do /')
+});
+
+module.exports = routes;
